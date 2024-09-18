b1f58f51035a6eb35813b16d1d93268ff174e468
Tim Musson <tim@troop2el.org>
2024-09-18 19:42:23 -0400

    adding an html file with a set of js

---
+++ EagleScoutAlumniData.js
@@ -0,0 +1,14 @@
+const EagleScoutAlumni = [ // 0 left spaces so it sorts to top with 'sort /r'
+	// 2 left spaces so these lines sort between top and bottom
+  ["2023-10-19, Budzyn, Joseph, Troop-2B"],
+  ["2022-05-19, Mitchell, Karen, Troop-2g"],
+  ["2021-06-16, Gresens, Gunnar, Troop-2B"],
+  ["2020-04-02, Mitchel, Charlie, Troop-2B"],
+  ["2020-03-05, Faust, Nathan, Troop-2B"],
+  ["2018-10-23, Reeves, Brian, Troop-2"],
+  ["2017-09-10, McNeilly, Nathaniel, Troop-2"],
+  ["2017-03-21, Musson, Liam, Troop-2"],
+  ["2014, Fitschen-Brown, Rhys, Troop-2"],
+  ["2014-08-14, Musson, Trevor, Troop-2"],
+  ["1923, Ford, Gaylon, Troop-2"]
+    ] // 4 left spaces so it sorts to bottom with 'sort /r'
